af3aa4576b7b238d157d79869fcda97722c6a6e9
svobovo17 <77615829+svobovo17@users.noreply.github.com>
2021-02-17 10:29:06 +0100

    Delete Project directory

--- Project/hlavni/vyhledani_uzivatele.php
+++
@@ -1,7 +0,0 @@
-<?php
-	session_start();
-	include "../library.php";
-	include "../db.php";
-	$_SESSION['vybrany_uzivatel'] = vyber_uzivatelu($_SESSION['uzivatel'], $_POST["search"]);
-	header('location:index.php');
-?>
